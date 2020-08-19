6e82c648a6dea43022fac2d57e8aafda18d98e46
Sergey <ss.vanteev@gmail.com>
2020-08-20 01:31:59 +0300

    added google auth and an ability to create messages with timestamps

--- src/Chat.js
+++ src/Chat.js
@@ -5,19 +5,31 @@ import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 import {useParams} from 'react-router-dom';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase';
 
 function Chat() {
     const [seed,setSeed]  = useState(''); 
     const [input,setInput] = useState('')
     const {roomId} = useParams();
     const [roomName,setRoomName] = useState('');
+    const [messages,setMessages] = useState([])
+    const [{user},dispatch] = useStateValue();
 
     useEffect(() => {
         if(roomId){
             db.collection('rooms').doc(roomId)
-            .onSnapShot(snapshot => (
-                setRoomName(snapshot.data().name)
-            ))
+            .onSnapshot((snapshot) => (
+                setRoomName(snapshot.data().name)));
+
+            db.collection('rooms').doc(roomId).
+            collection('messages').orderBy('timestamp','asc')
+            .onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map(doc => 
+                    doc.data()))
+            )
+                )
+
         }
     },[roomId])
 
@@ -28,8 +40,13 @@ function Chat() {
 
     const sendMessage = (e) => {
         e.preventDefault()
+        db.collection('rooms').doc(roomId).collection('messages').add({
+            message:input,
+            user:user.displayName,
+            timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+        })
         setInput('');
-    }
+    };
 
     return (
         <div className='chat'>
@@ -37,17 +54,21 @@ function Chat() {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className='chatheader__info'>
                     <h3>{roomName}</h3>
-                    <p>Last seen at...</p>
+                    <p> last seen {new Date(messages[messages.length-1]?.timestamp?.toDate()).toUTCString()}</p>
                 </div>
                 </div>
                 <div className='chat__body'>
-
+                    {messages.map(message => (
                         <p className=
                         {`chat_message ${true} && "chat_receiver"}`}>
-                        <span className='chat__name'>Sergey Vanteev</span>
+                        <span className='chat__name'>{message.name}</span>
+                        {message.message}
                         <span className='chat__timestamp'>
+                            {new Date(message.timestamp?.toDate()).toUTCString()}
                         </span>
                     </p>
+                    ))}
+                      
                 </div>
                     <div className='chat__footer'>
                         <InsertEmoticonIcon/>
